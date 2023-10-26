fa89d049cb7c76e234299c00142121fa95e4b44c
Hemant Prajapati <hemantprajapati20500@gmail.com>
2023-10-26 20:29:14 +0530

    array question started

---
+++ Javascript/Arrays/Problem1.js
@@ -0,0 +1,17 @@
+// Find the largest number in the array
+
+
+
+let arr=[2,5,1,3,0]
+
+let max=-Infinity;
+
+for(let i=0;i<arr.length;i++){
+  if(arr[i]>max){
+    max=arr[i];
+  }
+  
+  
+}
+
+console.log(max)
