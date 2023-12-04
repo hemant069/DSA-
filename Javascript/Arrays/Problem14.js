6ab283ed91e381e654916b603134785ecdf3d8ba
Hemant Prajapati <hemantprajapati20500@gmail.com>
2023-12-04 22:45:42 +0530

    longest subarray in the array which is equal to k

--- Javascript/Arrays/Problem14.js
+++ Javascript/Arrays/Problem14.js
@@ -1,38 +1,4 @@
 
-// Brute Force Approch
-
-
-let arr=[2,3,5,1,9];
-
-let leng=0;
-let k=10;
-
-for (let i = 0; i < arr.length; i++) {
-    
-    for(let j=i;j<arr.length;j++){
-      
-      let sum=0;
-      
-      for(let k=i;k<=j;k++){
-
-        sum+=arr[k];
-  
-        
-      }
-
-      if(sum==k){
-          leng=Math.max(leng,j-i+1);
-
-        }
-    }
-}
-
-console.log(leng)
-
- // 2. Better Solution
- 
- 
-
 let hashmap=new Map();
 
 let PrefixSum=0;
