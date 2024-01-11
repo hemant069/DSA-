ab624f98c9fc3b494a025780d72a69b7c2d55f3c
Hemant Prajapati <hemantprajapati20500@gmail.com>
2024-01-12 00:35:59 +0530

    lower bound

---
+++ Javascript/Binary Search/Problme2.js
@@ -0,0 +1,36 @@
+/// Lower Bound
+
+
+
+
+let arr=[3,5,8,15,19];
+
+let x=9;
+
+let ans=arr.length;
+
+let low=0;
+let high=arr.length-1;
+
+
+while(low<high){
+  let mid=Math.floor((low+high)/2);
+  
+  
+  if(arr[mid]>=x){
+  ans=mid;
+  high=mid-1
+   
+  }
+  
+  else{
+     low=mid+1;
+  }
+ 
+   
+  
+
+  
+}
+
+console.log(ans)
