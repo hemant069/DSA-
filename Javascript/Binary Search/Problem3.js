ebefcb01bc5d1668e1b32143c0dae11602f4e0a0
Hemant Prajapati <hemantprajapati20500@gmail.com>
2024-01-12 23:10:57 +0530

    upper bound is added

---
+++ Javascript/Binary Search/Problem3.js
@@ -0,0 +1,26 @@
+
+
+let arr=[3,5,8,9,15,19];
+
+let x=9;
+
+
+// Upper bound
+
+let low=0;
+let high=arr.length-1;
+let ans=arr.length;
+
+while(low<high){
+  let mid=Math.floor((low+high)/2);
+  
+  if(arr[mid]>x){
+    ans=mid;
+    high=mid-1;
+  }
+  else{
+    low=mid+1;
+  }
+}
+
+console.log(ans)
