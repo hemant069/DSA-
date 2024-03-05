207f44980597fd23134b5bcfa0e3bd7e8b3c217e
Hemant Prajapati <hemantprajapati20500@gmail.com>
2024-03-05 18:17:29 +0530

    isomarpic string

---
+++ Javascript/String/Problem5.js
@@ -0,0 +1,18 @@
+var isIsomorphic = function (s, t) {
+  let mapS = new Map();
+  let mapT = new Map();
+
+  for (let i = 0; i < s.length; i++) {
+    if (!mapS.has(s[i])) {
+      mapS.set(s[i], t[i]);
+    } else if (mapS.get(s[i]) !== t[i]) {
+      return false;
+    }
+    if (!mapT.has(t[i])) {
+      mapT.set(t[i], s[i]);
+    } else if (mapT.get(t[i]) !== s[i]) {
+      return false;
+    }
+  }
+  return true;
+};
